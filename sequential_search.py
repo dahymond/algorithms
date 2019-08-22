424fd17857c4187e3cd4892c62c4a3512dddb7dc
Solomon Sunday <ssdahymond@gmail.com>
2019-08-23 00:52:34 +0100

    add the ordered sequential search

--- sequential_search.py
+++ sequential_search.py
@@ -13,5 +13,26 @@ def seq_search(arr,  ele):
 
     return found
 
+def ordered_seq_search(arr,  ele):
+'''
+Input array must be ordered
+'''
+    pos = 0
+    found = False
+    stopped = False
+
+    while pos < len(arr) and not found:
+
+        if arr[pos]  == ele:
+            found  = True
+
+        else:
+            if arr[pos] > ele:
+                stopped = True
+
+            else:
+                pos += 1    
+    return found    
+
 print seq_search([1,2,3,4,5,6,7,8,9], 10)
 
