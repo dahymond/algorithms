c97caeb610a9d54245883f7e66228146bbc27232
Solomon Sunday <ssdahymond@gmail.com>
2019-08-25 13:42:25 +0100

    adding insertion_sort

---
+++ insertion_sort.py
@@ -0,0 +1,9 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+         currentvalue = arr[]
+         position = i
+
+         while position is 0 and arr[position-1] > currentvalue:
+
+             arr[position] = arr[position-1]
+             position = position-1 
