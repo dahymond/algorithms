43a14aa2558ee985b173a8b086e6903bd359e856
Solomon Sunday <ssdahymond@gmail.com>
2019-09-01 22:56:16 +0100

    added comments for easy undertanding

--- amazon_stock_prices.py
+++ amazon_stock_prices.py
@@ -1,17 +1,34 @@
+#Problem
+#You've been given a list of historical stock prices for a single day for Amazon stock. The index of the list represents the timestamp, so the element at index of 0 is the initial price of the stock, the element at index 1 is the next recorded price of the stock for that day, etc. Your task is to write a function that will return the maximum profit possible from the purchase and sale of a single share of Amazon stock on that day. Keep in mind to try to make this as efficient as possible.
+
+#For example, if you were given the list of stock prices:
+
+#prices = [12,11,15,3,10]
+
+#Then your function would return the maximum possible profit, which would be 7 (buying at 3 and selling at 10).
+
+
 def profit(stock_prices):
+    
+    # Start minimum price marker at first price
     min_stock_price = stock_prices[0]
-
+    
+    # Start off with a profit of zero
     max_profit = 0
-
+    
     for price in stock_prices:
-
-        min_stock_price = min(min_stock_price, price)
-
-        comparison_profit = price - min_stock_price  
-
-        max_profit = max(max_profit, comparison_profit)
-
-    return max_profit    
+        
+        # Check to set the lowest stock price so far
+        min_stock_price = min(min_stock_price,price)
+        
+        # Check the current price against our minimum for a profit 
+        # comparison against the max_profit
+        comparison_profit = price - min_stock_price
+        
+        # Compare against our max_profit so far
+        max_profit = max(max_profit,comparison_profit)
+        
+    return max_profit  
 
 
 print profit([23,12,3,10,25])
