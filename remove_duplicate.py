bfbd16823926c32b76e009050ccfb2226c72fd77
Solomon Sunday <ssdahymond@gmail.com>
2019-09-03 01:12:00 +0100

    added duplicate removal in an array or list

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -16,3 +16,28 @@ def rem_duplicate(words):
 
 
 print rem_duplicate('Solomon sunday')
+
+
+
+def duplicate_removal_list(arr):
+    
+    if len(arr) <= 1:
+        return ValueError
+
+    result = []
+
+    seen = set()
+
+    for numb in arr:
+
+        if numb not in seen:
+            seen.add(numb)
+            result.append(numb)
+
+
+    return result
+
+
+print duplicate_removal_list([1,2,3,4,4,4,5,6,8])
+
+print duplicate_removal_list([1,12,12,13,13,14,1,4,4,5,6,8])
